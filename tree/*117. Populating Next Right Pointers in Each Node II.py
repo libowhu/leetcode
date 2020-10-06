a13931c4c0bb5ee5a32ed3828b19b77f56e11f95
Bo Li <libo.whu@gmail.com>
2020-10-07 00:59:38 +0100

    117

---
+++ tree/*117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        start_of_level = root
+        while start_of_level:
+            cur_level_cur_node = start_of_level
+            next_level_pre_node = None
+            new_next_start_of_level = None
+            while cur_level_cur_node:
+                next_level_nodes = [cur_level_cur_node.left, cur_level_cur_node.right]
+                for node in next_level_nodes:
+                    if node:
+                        if not next_level_pre_node:
+                            next_level_pre_node = node
+                            start_of_level = node
+                        else:
+                            next_level_pre_node.next = node
+                            next_level_pre_node = node
+                cur_level_cur_node = cur_level_cur_node.next
+            if not next_level_pre_node:
+                start_of_level = None
+        return root
