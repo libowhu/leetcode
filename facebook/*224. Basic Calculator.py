cd914542fa136d7d8413059f813e3885c6bbcaaa
Bo Li <libo.whu@gmail.com>
2020-10-14 22:40:55 +0100

    update 224

--- facebook/*224. Basic Calculator.py
+++ facebook/*224. Basic Calculator.py
@@ -1,30 +1,33 @@
 # https://leetcode.com/problems/basic-calculator/
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        presign = 1
-        num = 0
-        sum = 0
+        def helper(q):
+            stack = []
+            num = ""
+            sign = "+"
 
-        for c in s + "+":
-            if c.isdigit():
-                num = num * 10 + int(c)
-            elif c in "+-":
-                if presign == 1:
-                    sum += num * 1
-                if presign == -1:
-                    sum += num * -1
-                presign = 1 if c == "+" else -1
-                num = 0
-            elif c in '()':
-                if c == "(":
-                    stack.append((sum, presign))
-                    sum = 0
-                if c == ")":
-                    sum += presign * num
-                    pre_sum, pre_presign = stack.pop()
-                    sum = pre_sum + pre_presign * sum
-                num = 0
-                presign = 1
+            while q:
+                c = q.popleft()
+                if c.isdigit():
+                    num += c
+                elif c in "-+":
+                    if sign == "+":
+                        stack.append(int(num))
+                    if sign == "-":
+                        stack.append(-int(num))
+                    sign = c
+                    num = ""
+                elif c == "(":
+                    num = helper(q)
+                elif c == ")":
+                    if sign == "+":
+                        stack.append(int(num))
+                    if sign == "-":
+                        stack.append(-int(num))
+                    break
 
-        return sum
+            return sum(stack)
+
+        s = s.replace(" ", "")
+        q = collections.deque(list(s + '+'))
+        return helper(q)
