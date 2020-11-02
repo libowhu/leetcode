7c697a3ec0726e106f9ecf8f2c2bfca54243fd24
Bo Li <libo.whu@gmail.com>
2020-11-02 10:49:40 +0000

    50

---
+++ binarysearch/*50. Pow(x, n).py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/powx-n/
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        if n % 2 == 0:
+            return self.myPow(x * x, n // 2)
+        else:
+            return self.myPow(x * x, n // 2) * x
