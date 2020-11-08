02636a2c487f65a733249683425cb9c839cda5e7
Bo Li <libo.whu@gmail.com>
2020-11-08 02:48:49 +0000

    add 658

---
+++ binarysearch/*658. Find K Closest Elements.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-k-closest-elements/
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        n = len(arr)
+        l, r = 0, n - k
+        while l < r:
+            m = l + (r - l) // 2
+            if x - arr[m] > arr[m + k] - x:
+                l = m + 1
+            else:
+                r = m
+        return arr[l:l + k]
+
