a90ca7227b0e19eb1572816196d0c693a426cac3
Bo Li <libo.whu@gmail.com>
2020-11-14 21:36:08 +0000

    add second solution for 210

--- graph/210. Course Schedule II.py
+++ graph/210. Course Schedule II.py
@@ -29,3 +29,50 @@ class Solution:
                         queue.append(v)
 
         return output if len(output) == numCourses else []
+
+
+#Output all paths
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = {i: set() for i in range(numCourses)}
+        degree = {i: 0 for i in range(numCourses)}
+
+        for pre in prerequisites:
+            chil, par = pre
+            graph[chil].add(par)
+            degree[chil] += 1
+
+        queue = collections.deque([])
+
+        for v in degree:
+            if degree[v] == 0:
+                queue.append(v)
+
+        if not queue: return []
+
+        def backtracking(starts, temp, output):
+            if not starts:
+                if len(temp) == numCourses:
+                    output.append(temp.copy())
+                return
+
+            for start in starts:
+                temp.append(start)
+                new_starts = collections.deque(starts)
+                new_starts.remove(start)
+                for vertex in graph:
+                    if start in graph[vertex]:
+                        degree[vertex] -= 1
+                        if degree[vertex] == 0:
+                            new_starts.append(vertex)
+
+                backtracking(new_starts, temp, output)
+                temp.pop()
+
+                for vertex in graph:
+                    if start in graph[vertex]:
+                        degree[vertex] += 1
+
+        result = []
+        backtracking(queue, [], result)
+        return result
