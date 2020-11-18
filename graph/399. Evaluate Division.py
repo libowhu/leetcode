81beab050b00f8dd16194cb443e52d6a3428a0fc
Bo Li <libo.whu@gmail.com>
2020-11-18 20:20:43 +0000

    add 399

--- graph/399. Evaluate Division.py
+++ graph/399. Evaluate Division.py
@@ -7,26 +7,24 @@ class Solution:
             graph[start].add((end, value))
             graph[end].add((start, 1.0 / value))
 
-        def helper(graph, pre, start, end, temp, visited):
+        def dfs(graph, start, end, temp, visited):
             if start not in graph or end not in graph:
                 return -1.0
             if start == end:
                 return temp
-            if start != end and len(graph[start]) == 1 and pre != "":
-                return -1.0
             for cur, value in graph[start]:
                 if cur not in visited:
                     visited.add(cur)
-                    val = helper(graph, start, cur, end, temp * value, visited)
+                    output = dfs(graph, cur, end, temp * value, visited)
                     visited.remove(cur)
-                    if val != -1.0:
-                        return val
+                    if output != -1.0:
+                        return output
             return -1.0
 
         output = []
         for query in queries:
             start, end = query
             visited = {start}
-            val = helper(graph, "", start, end, 1.0, visited)
+            val = dfs(graph, start, end, 1.0, visited)
             output.append(val)
         return output
