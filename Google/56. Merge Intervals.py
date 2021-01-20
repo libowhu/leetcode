fe26aef54c50f10d0af8012de7d4e354949fc171
Bo Li <libo.whu@gmail.com>
2021-01-20 23:27:29 +0000

    add 57

--- Google/56. Merge Intervals.py
+++ Google/56. Merge Intervals.py
@@ -1,14 +1,15 @@
 # https://leetcode.com/problems/merge-intervals/
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        sorted_intervals = sorted(intervals, key=lambda x: x[0])
-        cur_interval = sorted_intervals[0]
-        output = []
-        for interval in sorted_intervals[1:]:
-            if max(cur_interval[0], interval[0]) <= min(cur_interval[1], interval[1]):
-                cur_interval = [min(cur_interval[0], interval[0]), max(cur_interval[1], interval[1])]
+        res = []
+        for interval in sorted(intervals, key=lambda x: x[0]):
+            if len(res) == 0:
+                res.append(interval)
+                continue
+            if res[-1][1] < interval[0]:
+                res.append(interval)
             else:
-                output.append(cur_interval)
-                cur_interval = interval
-        output.append(cur_interval)
-        return output
+                res[-1][1] = max(interval[1], res[-1][1])
+        return res
+
+
---
+++ Google/57. Insert Interval.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/insert-interval/
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # nlogn
+        newIntervals = sorted(intervals + [newInterval], key=lambda x:x[0])
+        res = [newIntervals[0]]
+        for interval in newIntervals[1:]:
+            if res[-1][1] < interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+        return res
+
+        # n
+        res = []
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                res.append(interval)
+            elif interval[0] > newInterval[1]:
+                res.append(newInterval)
+                newInterval = interval
+            elif interval[1] >= newInterval[0] or interval[0] >= newInterval[1]:
+                newInterval = [min(interval[0], newInterval[0]), max(interval[1], newInterval[1])]
+        res.append(newInterval)
+        return res
