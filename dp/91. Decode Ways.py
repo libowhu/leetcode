34ed352cbfd7a3b61f9d413034f1c94fda5340f9
Bo Li <libo.whu@gmail.com>
2021-02-13 21:09:50 +0000

    update 91

--- dp/91. Decode Ways.py
+++ dp/91. Decode Ways.py
@@ -18,3 +18,25 @@ class Solution:
             cache[string] = res
             return res
         return backtracking(s)
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        def helper(s):
+            n = len(s)
+            dp = [0] * (n + 1)
+
+            dp[0] = 1
+            dp[1] = 1 if s[0] != '0' else 0
+
+            for i in range(2, n + 1):
+                str1 = s[i - 1:i]
+                str2 = s[i - 2:i]
+                if str1 and not str1.startswith("0") and 1 <= int(str1) <= 9:
+                    dp[i] += dp[i - 1]
+
+                if str2 and not str2.startswith("0") and 10 <= int(str2) <= 26:
+                    dp[i] += dp[i - 2]
+            return dp[n]
+
+        return helper(s)
